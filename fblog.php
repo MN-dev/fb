838eeb85de841485ca3d939d5e3f6980b03e0fc1
hunter eyes eg <wadedy2020@gmail.com>
2016-05-31 22:38:25 +0200

    Create fblog.php

---
+++ fblog.php
@@ -0,0 +1,22 @@
+<?php
+$login_email = '';
+$login_pass = '';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://mobile.facebook.com/login.php');
+curl_setopt($ch, CURLOPT_POSTFIELDS,'email='.urlencode($login_email).'&pass='.urlencode($login_pass).'&login=Login');
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_COOKIEJAR, "cookies.txt");
+curl_setopt($ch, CURLOPT_COOKIEFILE, "cookies.txt");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.3) Gecko/20070309 Firefox/2.0.0.3");
+curl_setopt($ch, CURLOPT_REFERER, "http://mobile.facebook.com");
+
+$page = curl_exec($ch) or die(curl_error($ch));
+/*
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2); 
+*/
+echo $page;
+?>
